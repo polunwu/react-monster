479a247fdab4f9b2bd93a7b9fd79daf903755b03
polunwu <polunwu@gmail.com>
2020-09-15 20:42:42 +0800

    Updates

---
+++ precache-manifest.ffc9d25bdbc627cb43cb1b9f7540c3d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b71f170dd2ff24f3a108591d6477a4",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "531bbc1d393aad1d218b",
+    "url": "/react-monster/static/css/main.ea69c27e.chunk.css"
+  },
+  {
+    "revision": "397d3a8cbcc1827678c8",
+    "url": "/react-monster/static/js/2.d13e6d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster/static/js/2.d13e6d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531bbc1d393aad1d218b",
+    "url": "/react-monster/static/js/main.bcb07b5e.chunk.js"
+  },
+  {
+    "revision": "fede3969131bca5996ef",
+    "url": "/react-monster/static/js/runtime-main.0b6aa78c.js"
+  }
+]);
