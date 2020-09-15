649e4f64fcb405176b4316a99f7375b7c0d0d6a7
polunwu <polunwu@gmail.com>
2020-09-15 18:40:59 +0800

    Add filter state feature in render function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
     super()
     this.state = {
       monsters: [],
+      searchField: '',
     }
   }
 
@@ -21,9 +22,19 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(searchField.toLowerCase())
+    })
+
     return (
       <div className="App">
-        <CardList monsters={ this.state.monsters } />
+        <input 
+          type="search" 
+          placeholder="search for monsters" 
+          onChange={ e => this.setState({ searchField: e.target.value }) } 
+        />
+        <CardList monsters={ filteredMonsters } />
       </div>
     )
   }
